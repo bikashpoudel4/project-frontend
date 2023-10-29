0aa9919484d22d1ad9bd2bb6d2bccd73266e6626
Bikash Poudel <bikashpoudel4@gmail.com>
2023-10-29 19:22:20 +0100

    JS Modal Windows

--- JavaScript/Modal Window/script.js
+++ JavaScript/Modal Window/script.js
@@ -5,7 +5,28 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 // querySelectorAll for the same class name in multiple elems
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+// console.log(btnsOpenModal);
+
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+  // console.log(e.key);
+
+  if(e.key === 'Escape' && !modal.classList.contains('hidden')){    
+      closeModal();   
+  }
+});
